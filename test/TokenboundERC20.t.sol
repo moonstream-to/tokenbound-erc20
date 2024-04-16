2b95088cc8a49cc362bb1656a969d53398ea2966
Neeraj Kashyap <nkashy1@gmail.com>
2024-04-16 16:37:18 -0700

    Basic contracts are now working

--- test/TokenboundERC20.t.sol
+++ test/TokenboundERC20.t.sol
@@ -4,34 +4,130 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 
 import {ERC6551Registry} from "../lib/erc6551/src/ERC6551Registry.sol";
-import {AccountV3} from "../lib/tba/src/AccountV3.sol";
-import {AccountGuardian} from "../lib/tba/src/AccountGuardian.sol";
-import {Multicall3} from "../lib/tba/lib/multicall-authenticated/src/Multicall3.sol";
+import {ERC6551Account} from "../lib/erc6551/src/examples/simple/ERC6551Account.sol";
+
+import {ERC20BurnableWithSingleMinter, ERC721WithTokenboundERC20} from "../src/TokenboundERC20.sol";
+
+contract MockERC721WithTokenboundERC20 is ERC721WithTokenboundERC20 {
+    constructor(address tbaRegistryAddress, address tbaImplementationAddress) ERC721WithTokenboundERC20("test", "test", tbaRegistryAddress, tbaImplementationAddress, 0) {}
+
+    uint256 public supply;
+
+    function mint(address to, uint256 tokenId) public {
+        _mintAndDeployTBAAndTBERC20(to, tokenId, bytes32(0), "test", "test");
+        supply += 1;
+    }
+}
 
 
-contract CounterTest is Test {
+contract TokenboundERC20Tests is Test {
     ERC6551Registry private registry;
-    AccountV3 private accountImplementation;
+    ERC6551Account private accountImplementation;
+    MockERC721WithTokenboundERC20 private nfts;
+
+    uint256 private player1PrivateKey = 0x1;
+    uint256 private player2PrivateKey = 0x2;
+    uint256 private player3PrivateKey = 0x3;
+
+    address private player1 = vm.addr(player1PrivateKey);
+    address private player2 = vm.addr(player2PrivateKey);
+    address private player3 = vm.addr(player3PrivateKey);
 
     function setUp() public {
         registry = new ERC6551Registry();
-
-        accountImplementation = new AccountV3();
+        accountImplementation = new ERC6551Account();
+        nfts = new MockERC721WithTokenboundERC20(address(registry), address(accountImplementation));
     }
-    // Counter public counter;
 
-    // function setUp() public {
-    //     counter = new Counter();
-    //     counter.setNumber(0);
-    // }
+    function test_mint_nft() public {
+        uint256 tokenId = nfts.supply() + 1;
+        nfts.mint(player1, tokenId);
 
-    // function test_Increment() public {
-    //     counter.increment();
-    //     assertEq(counter.number(), 1);
-    // }
+        assertEq(nfts.ownerOf(tokenId), player1);
 
-    // function testFuzz_SetNumber(uint256 x) public {
-    //     counter.setNumber(x);
-    //     assertEq(counter.number(), x);
-    // }
+        assertEq(registry.account(address(accountImplementation), bytes32(0), block.chainid, address(nfts), tokenId), nfts.tba(tokenId));
+        assertGt(address(nfts.tba(tokenId)).code.length, 0);
+        assertGt(address(nfts.tberc20(tokenId)).code.length, 0);
+    }
+
+    function test_tberc20_mint_through_tba_as_nft_owner() public {
+        uint256 tokenId = nfts.supply() + 1;
+        nfts.mint(player1, tokenId);
+        ERC20BurnableWithSingleMinter tberc20 = ERC20BurnableWithSingleMinter(nfts.tberc20(tokenId));
+        ERC6551Account tba = ERC6551Account(payable(nfts.tba(tokenId)));
+
+        assertEq(tberc20.balanceOf(player3), 0);
+
+        vm.prank(player1);
+        tba.execute(address(tberc20), 0, abi.encodeWithSelector(tberc20.mint.selector, player3, 100), 0);
+        vm.stopPrank();
+
+        assertEq(tberc20.balanceOf(player3), 100);
+    }
+
+    function testRevert_tberc20_mint_directly_as_nft_owner() public {
+        uint256 tokenId = nfts.supply() + 1;
+        nfts.mint(player1, tokenId);
+        ERC20BurnableWithSingleMinter tberc20 = ERC20BurnableWithSingleMinter(nfts.tberc20(tokenId));
+
+        assertEq(tberc20.balanceOf(player3), 0);
+
+        vm.prank(player1);
+        vm.expectRevert(abi.encodeWithSelector(ERC20BurnableWithSingleMinter.InvalidMinter.selector, player1));
+        tberc20.mint(player3, 100);
+        vm.stopPrank();
+
+        assertEq(tberc20.balanceOf(player3), 0);
+    }
+
+    function testRevert_tberc20_mint_through_tba_as_ex_nft_owner() public {
+        uint256 tokenId = nfts.supply() + 1;
+        nfts.mint(player1, tokenId);
+        ERC20BurnableWithSingleMinter tberc20 = ERC20BurnableWithSingleMinter(nfts.tberc20(tokenId));
+        ERC6551Account tba = ERC6551Account(payable(nfts.tba(tokenId)));
+
+        assertEq(tberc20.balanceOf(player3), 0);
+
+        vm.prank(player1);
+        nfts.transferFrom(player1, player2, tokenId);
+        vm.expectRevert("Invalid signer");
+        tba.execute(address(tberc20), 0, abi.encodeWithSelector(tberc20.mint.selector, player3, 100), 0);
+        vm.stopPrank();
+
+        assertEq(tberc20.balanceOf(player3), 0);
+    }
+
+    function testRevert_tberc20_mint_through_tba_as_nonowner() public {
+        uint256 tokenId = nfts.supply() + 1;
+        nfts.mint(player1, tokenId);
+        ERC20BurnableWithSingleMinter tberc20 = ERC20BurnableWithSingleMinter(nfts.tberc20(tokenId));
+        ERC6551Account tba = ERC6551Account(payable(nfts.tba(tokenId)));
+
+        assertEq(tberc20.balanceOf(player3), 0);
+
+        vm.prank(player2);
+        vm.expectRevert("Invalid signer");
+        tba.execute(address(tberc20), 0, abi.encodeWithSelector(tberc20.mint.selector, player3, 100), 0);
+        vm.stopPrank();
+
+        assertEq(tberc20.balanceOf(player3), 0);
+    }
+
+    function test_tberc20_mint_through_tba_as_new_nft_owner() public {
+        uint256 tokenId = nfts.supply() + 1;
+        nfts.mint(player1, tokenId);
+        ERC20BurnableWithSingleMinter tberc20 = ERC20BurnableWithSingleMinter(nfts.tberc20(tokenId));
+        ERC6551Account tba = ERC6551Account(payable(nfts.tba(tokenId)));
+
+        assertEq(tberc20.balanceOf(player3), 0);
+
+        vm.prank(player1);
+        nfts.transferFrom(player1, player2, tokenId);
+
+        vm.prank(player2);
+        tba.execute(address(tberc20), 0, abi.encodeWithSelector(tberc20.mint.selector, player3, 100), 0);
+        vm.stopPrank();
+
+        assertEq(tberc20.balanceOf(player3), 100);
+    }
 }
