43170784824869c07e366d2632170ab5dad0f567
Neeraj Kashyap <nkashy1@gmail.com>
2024-04-16 15:48:12 -0700

    WIP - committing to work on the go

--- src/TokenboundERC20.sol
+++ src/TokenboundERC20.sol
@@ -1,14 +1,58 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+import {IERC6551Registry} from "../lib/erc6551/src/ERC6551Registry.sol";
+import {ERC20} from "../lib/openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC721} from "../lib/openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract ERC20BurnableWithSingleMinter is ERC20 {
+    address public minter;
+    uint8 private _decimals;
+
+    error InvalidMinter(address sender);
+
+    constructor(string memory name_, string memory symbol_, uint8 decimals_, address _minter) ERC20(name_, symbol_) {
+        _decimals = decimals_;
+        minter = _minter;
     }
 
-    function increment() public {
-        number++;
+    function decimals() public view override returns (uint8) {
+        return uint8(_decimals);
+    }
+
+    function burn(uint256 value) external {
+        return _burn(msg.sender, value);
+    }
+
+    function mint(address account, uint256 value) external {
+        if (msg.sender != minter) {
+            revert InvalidMinter(msg.sender);
+        }
+        _mint(account, value);
+    }
+}
+
+contract ERC721WithTokenboundERC20 is ERC721 {
+    address public tbaRegistryAddress;
+    address public tbaImplementationAddress;
+    IERC6551Registry private tbaRegistry;
+    uint8 public decimals;
+
+    mapping(uint256 => address) public tba;
+    mapping(uint256 => address) public tberc20;
+
+    constructor (string memory name_, string memory symbol_, address _tbaRegistryAddress, address _tbaImplementationAddress, uint8 _decimals) ERC721(name_, symbol_) {
+        tbaRegistryAddress = _tbaRegistryAddress;
+        tbaImplementationAddress = _tbaImplementationAddress;
+        tbaRegistry = IERC6551Registry(_tbaRegistryAddress);
+        decimals = _decimals;
+    }
+
+    function _mintAndDeployTBAAndTBERC20(address to, uint256 tokenId, bytes32 salt, string memory name_, string memory symbol_) internal returns (address, address) {
+        tba[tokenId] = tbaRegistry.createAccount(tbaImplementationAddress, salt, block.chainid, address(this), tokenId);
+        ERC20BurnableWithSingleMinter tberc20Contract = new ERC20BurnableWithSingleMinter{salt: salt}(name_, symbol_, decimals, tba[tokenId]);
+        tberc20[tokenId] = address(tberc20Contract);
+        _mint(to, tokenId);
+        return (tba[tokenId], tberc20[tokenId]);
     }
 }
--- test/TokenboundERC20.t.sol
+++ test/TokenboundERC20.t.sol
@@ -2,23 +2,36 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/TokenboundERC20.sol";
+
+import {ERC6551Registry} from "../lib/erc6551/src/ERC6551Registry.sol";
+import {AccountV3} from "../lib/tba/src/AccountV3.sol";
+import {AccountGuardian} from "../lib/tba/src/AccountGuardian.sol";
+import {Multicall3} from "../lib/tba/lib/multicall-authenticated/src/Multicall3.sol";
+
 
 contract CounterTest is Test {
-    Counter public counter;
+    ERC6551Registry private registry;
+    AccountV3 private accountImplementation;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
-    }
+        registry = new ERC6551Registry();
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+        accountImplementation = new AccountV3();
     }
+    // Counter public counter;
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
-    }
+    // function setUp() public {
+    //     counter = new Counter();
+    //     counter.setNumber(0);
+    // }
+
+    // function test_Increment() public {
+    //     counter.increment();
+    //     assertEq(counter.number(), 1);
+    // }
+
+    // function testFuzz_SetNumber(uint256 x) public {
+    //     counter.setNumber(x);
+    //     assertEq(counter.number(), x);
+    // }
 }
